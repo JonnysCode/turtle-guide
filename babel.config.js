e20f455b6c9d605cd899249d2f3e53ecd9fd385c
Jonathan Gruss <63511216+JonnysCode@users.noreply.github.com>
2025-06-25 19:57:02 +0200

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,8 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }]
+      ['babel-preset-expo', { jsxImportSource: 'nativewind', runtime: 'automatic' }],
+      'nativewind/babel',
     ],
     plugins: [
       'react-native-reanimated/plugin', // This should be last
